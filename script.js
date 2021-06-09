5117f185b3d4aaab33b43cb7800f2bc2c745f60e
Prasanna Dangol <1905915@kiit.ac.com>
2021-06-09 18:18:38 +0545

    ver 1.2

--- script.js
+++ script.js
@@ -1,30 +1,57 @@
 console.log("This is magic notes");
 
+//Creating an element "Write here..."
 let element = document.createElement("p");
-element.setAttribute("class", "val");
-element.setAttribute("id", "val");
 element.setAttribute("style", "font-size: 30px; font-weight:100 ")
 element.innerText = "write here...";
-let txtchange = document.querySelector(".inputs"); //box
-txtchange.appendChild(element);
+let inputbox = document.querySelector(".inputs"); //box
+inputbox.appendChild(element);
+
+let clip = document.querySelector(".clip");
+inputbox.addEventListener("click", function() {
+    document.querySelector(".nheading").style.textDecoration = "underline dotted";
+    document.querySelector(".nheading").style.color = "#000000ee";
+    inputbox.style.transform = "rotate(0deg)";
+    clip.style.marginTop = "3.4rem";
+    clip.style.marginLeft = "-1rem";
+    let noTextAreas = document.getElementsByClassName("textarea").length;
+    if (noTextAreas == 0) {
+        let html = "";
+        element.innerHTML = `<textarea class= "textarea form-control" id="textarea" rows="3" maxlength= "130">${html}</textarea>`;
+    }
+})
 
 
-element.addEventListener("click", function() {
-        document.querySelector(".nheading").style.textDecoration = "underline dotted";
-        document.querySelector(".nheading").style.color = "#000000ee";
-        txtchange.style.transform = "rotate(0deg)";
-        let noTextAreas = document.getElementsByClassName("textarea").length;
-        if (noTextAreas == 0) {
-            let html = element.innerHTML;
-            element.innerHTML = `<textarea class= "textarea form-control" id="textarea" rows="3">${html}</textarea>`;
-        }
 
-    })
-    //note count
+//note count
 var ncount = 1;
 let addbtn = document.querySelector(".add_note");
 let number = document.querySelector(".number");
-addbtn.addEventListener("click", function() {
+number.innerText = ncount;
+
+
+addbtn.addEventListener("click", function(e) {
+    showNotes();
+    document.getElementById("textarea").value = '';
     ncount += 1;
     number.innerText = ncount;
+
 })
+
+function showNotes() {
+    let texts = document.getElementById("textarea").value;
+    let prntarea = document.querySelector(".printarea");
+    let printer = document.createElement("div");
+    printer.setAttribute("class", "printer");
+    printer.innerHTML = "Note #" + ncount + "<br>" + texts;
+    prntarea.appendChild(printer);
+
+}
+
+
+
+// number.innerText = ncount;
+// document.querySelector(".note_save").innerText = texts;
+// document.querySelector(".noteno").innerText = (ncount - 1);
+// document.querySelector(".printer").style.display = "flex";
+// // localStorage.setItem("NoteNumber", ncount);
